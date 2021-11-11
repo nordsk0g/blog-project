6e1492b3180281e3f8ad5be69c8fba6e9855eb5f
Steve Christian <steven.tr.christian@gmail.com>
2021-11-11 14:05:38 +0100

    added firebase integration to getPosts composable

--- src/composables/getPosts.js
+++ src/composables/getPosts.js
@@ -1,4 +1,5 @@
 import { ref } from '@vue/reactivity'
+import { projectFirestore } from '../firebase/config'
 
 const getPosts = () => {
   const posts = ref([])
@@ -6,12 +7,11 @@ const getPosts = () => {
 
     const load = async () => {
       try {
-        let data = await fetch('http://localhost:3000/posts')
-        if (!data.ok) {
-          throw Error('no data available')
-        } 
-        posts.value = await data.json()
+        const res = await projectFirestore.collection('posts').get()
 
+        posts.value = res.docs.map(doc => {
+          return { ...doc.data(), id: doc.id }
+        })
       } catch (err) {
         error.value = err.message
         console.log(error.value)
